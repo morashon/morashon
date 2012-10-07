c9827ff79fe38a26d4e388b902941dffb1450a4f
Morashon <@>
2012-10-06 22:02:36 -0700

    format for fun & proft

--- score/dialog/freq.py
+++ score/dialog/freq.py
@@ -2,8 +2,8 @@
 import math
 import matplotlib.mlab as mat
 
-N = 64
-P = 5
+N = 128
+P = 16
 mul = (2 * math.pi) / P 
 data = []
 for i in range(N):
@@ -20,12 +20,14 @@ print "Y axis (freqs):", list(spec[1])
 print "data:"
 print
 for i in range(len(spec[0])-1, -1, -1):
-    print "%2.4f" % spec[1][i], "|",
+    s = "%3.3f" % (2.0 / spec[1][i]) if spec[1][i] else "DC "
+    s = "%7s" % s
+    print s, "|",
     for j in range(len(spec[2])):
         print "%1.4f" % spec[0][i][j],
     print
-print "-" * (7 * (len(spec[2]) + 1) + 1)
-print "times: | ",
+print "-" * (7 * (len(spec[2]) + 1) + 2)
+print "times:  | ",
 for j in range(len(spec[2])):
     print "%2.3f" % spec[2][j],
 print
