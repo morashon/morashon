4f13de89775a2fbf9f9a12046a3a986086564868
Morashon <@>
2012-10-06 21:45:01 -0700

    basic freq plot

--- score/dialog/freq.py
+++ score/dialog/freq.py
@@ -1,5 +1,6 @@
 #testing some frequency analysis code
 import math
+import matplotlib.mlab as mat
 
 N = 64
 P = 32
@@ -7,4 +8,24 @@ mul = (2 * math.pi) / P
 data = []
 for i in range(N):
     val = math.sin(i * mul)
+    data.append(val)
     print "%3d %+2.8f" % (i, val), "|", " " * int(52 + math.floor(val * 50 + 0.5)) + "."
+##print data
+
+print
+print
+spec = mat.specgram(data, NFFT=32, noverlap = 16)
+print "X axis (times):", list(spec[2])
+print "Y axis (freqs):", list(spec[1])
+print "data:"
+print
+for i in range(len(spec[0])-1, -1, -1):
+    print "%2.4f" % spec[1][i], "|",
+    for j in range(len(spec[2])):
+        print "%1.4f" % spec[0][i][j],
+    print
+print "-" * (7 * (len(spec[2]) + 1) + 1)
+print "times: | ",
+for j in range(len(spec[2])):
+    print "%2.3f" % spec[2][j],
+print
