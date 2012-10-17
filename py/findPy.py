1178e0b4d38fba40ab8cf5d9a241de167648c390
Morashon <@>
2012-10-16 23:32:06 -0700

    cleaner way to deal with paths

--- py/findPy.py
+++ py/findPy.py
@@ -1,8 +1,11 @@
-import os
+import os, sys
 def findPy(p):
-    for e in ("./", "../", "../../"):
-        for f in ("", "py/"):
-            if os.path.exists(e + f + p):
-                return e + f + p
-    print "cannot find", p, "anywhere"
-    exit()
+    return os.path.dirname(os.path.abspath(sys.argv[0])) + "/" + p
+
+##def findPy(p):
+##    for e in ("./", "../", "../../"):
+##        for f in ("", "py/"):
+##            if os.path.exists(e + f + p):
+##                return e + f + p
+##    print "cannot find", p, "anywhere"
+##    exit()
