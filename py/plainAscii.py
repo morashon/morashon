d3e36d9eae58953b3fa914195504f3b1fa81409b
Morashon <@>
2012-10-22 02:04:14 -0700

    clean up

--- py/plainAscii.py
+++ py/plainAscii.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys, os, codecs
 
 f = codecs.open(sys.argv[1], 'r', "utf-8")
@@ -12,7 +13,7 @@ for c in s:
     if ord(c) > 127:
         print "bad char at", i, hex(ord(c))
         if ord(c) == 0xa0:
-            c = "0x0a"
+            c = chr(0x0a)
     f.write(c)
     i += 1
 
