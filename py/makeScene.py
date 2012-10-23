f654dfa5e25b54fc77fc18d1f3a3474943ba2451
Morashon <@>
2012-10-22 21:29:56 -0700

    modularize my sun

--- py/makeScene.py
+++ py/makeScene.py
@@ -17,6 +17,145 @@ from ordereddict import OrderedDict
 BUILDPARTS = False
 BUILDJUST = None
 BUILDDIR = "build"
+
+def main(scene):
+    f = open(scene)
+    lines = f.readlines()
+    f.close()
+    scene = scene[:-4]
+    index = 0
+    actors = {}
+    files = OrderedDict()
+    os.chdir(BUILDDIR)
+
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        line = line.strip()
+        if line == "":
+            i += 1
+            continue
+
+        if ":" in line and "{" in line:        
+            if line.find(":") < line.find("{"):                     #inline actor definition
+                actor, parameters = line.split(":")
+                actors[actor.strip()] = parameters.strip()
+            else:                                                   #include file
+                a, fn = line.split(":")
+                f = open("../" + fn.strip().replace("}",""))
+                for ln in f.readlines():
+                    lines.insert(i + 1, ln)
+                f.close()
+            i += 1
+            continue
+
+        actor = None
+        if "{" in line:
+            extra = ""
+            temp = line[1:].replace("}", "")
+            if ";" in temp:                                         #extra parameters for these lines (until next actor spec)
+                extra = temp[temp.find(";") + 1:]
+                temp = temp[:temp.find(";")]
+            if temp in actors:
+                actor = temp
+                line = actors[actor]
+                if extra:
+                    line = line.replace("}","") + ";" + extra + "}"
+                name = scene + "_" + str(index) + "_" + actor + ".txt"
+                index += 1
+                files[name] = ""
+        files[name] += line + "\n"
+        i += 1
+
+    buildMaster = False
+    errors = 0
+    index = -1
+    for fil in files:
+        index += 1
+    ##    print "-----------------------", fil
+    ##    print files[fil],
+        rewrite = False
+        if os.path.exists(fil):
+            f = open(fil)
+            s = f.read()
+            f.close()
+            if s != files[fil]:
+                rewrite = True
+        else:
+            rewrite = True
+
+        if BUILDJUST != None:
+            if type(BUILDJUST) == type(0):
+                rewrite = BUILDJUST == index
+            else:
+                rewrite = BUILDJUST == fil
+
+        if rewrite:
+            buildMaster = True
+            print "-------->", fil, "is new or has changed -- writing to disk:"
+            print files[fil]
+            f = open(fil, "w")
+            f.write(files[fil])
+            f.close()
+            print "   rebuilding wav file"
+            wav = fil[:-4] + ".wav"
+            cmd = "rm " + wav
+            print cmd
+            os.system(cmd)
+            cmd = text2vox + " " + fil + " " + wav + " >> " + scene + ".log"
+            print cmd
+            os.system(cmd)
+            if not os.path.exists(wav):
+                print "***ERROR*** failed to build", wav, "-- renaming to force rebuild"
+                cmd = "mv " + fil + " " + fil[:-4] + "_error.txt"
+                print cmd
+                os.system(cmd)
+                errors += 1
+            else:
+                if CHANGES:
+                    cmd = "mplayer " + fil[:-4] + ".wav"
+                    print cmd
+                    os.system(cmd)
+        else:
+            if BUILDJUST == None:
+                print "++++++++>", fil, "is unchanged"
+
+    if (BUILDJUST == None) and (buildMaster and errors == 0):
+        print "Building master wav file"
+        cmd = "sox "
+        for fil in files:
+            print fil
+            cmd += fil[:-4] + ".wav "
+        cmd += scene + ".wav"
+        print cmd
+        os.system(cmd)
+
+        if BUILDPARTS:
+            print "Building parts for each actor"
+            for actor in actors:
+                someAudio = False
+                cmd = "sox "
+                for fil in files:
+                    if not "_" + actor + ".txt" in fil:
+                        cmd += "-v 0 "
+                    else:
+                        someAudio = True
+                    cmd += fil[:-4] + ".wav "
+                cmd += scene + "_" + actor + ".wav"
+                if someAudio:
+                    print cmd
+                    os.system(cmd)
+
+    if errors:
+        print errors, "errors encountered -- will not rebuild master"
+    else:
+        if PLAY:
+            cmd = "mplayer " + scene + ".wav"
+            if type(PLAY) == type(0):
+                cmd += " -ss " + str(PLAY)
+            print cmd
+            os.system(cmd)
+
 if not os.path.exists(BUILDDIR):
     cmd = "mkdir " + BUILDDIR
     print cmd
@@ -52,140 +191,4 @@ for e in sys.argv[1:]:
         scene = e
 print "BUILDJUST:", BUILDJUST, "CHANGES:", CHANGES, "PLAY:", PLAY
 
-
-f = open(scene)
-lines = f.readlines()
-f.close()
-scene = scene[:-4]
-index = 0
-actors = {}
-files = OrderedDict()
-os.chdir(BUILDDIR)
-
-i = 0
-while i < len(lines):
-    line = lines[i]
-    line = line.strip()
-    if line == "":
-        i += 1
-        continue
-
-    if ":" in line and "{" in line:        
-        if line.find(":") < line.find("{"):                     #inline actor definition
-            actor, parameters = line.split(":")
-            actors[actor.strip()] = parameters.strip()
-        else:                                                   #include file
-            a, fn = line.split(":")
-            f = open("../" + fn.strip().replace("}",""))
-            for ln in f.readlines():
-                lines.insert(i + 1, ln)
-            f.close()
-        i += 1
-        continue
-
-    actor = None
-    if "{" in line:
-        extra = ""
-        temp = line[1:].replace("}", "")
-        if ";" in temp:                                         #extra parameters for these lines (until next actor spec)
-            extra = temp[temp.find(";") + 1:]
-            temp = temp[:temp.find(";")]
-        if temp in actors:
-            actor = temp
-            line = actors[actor]
-            if extra:
-                line = line.replace("}","") + ";" + extra + "}"
-            name = scene + "_" + str(index) + "_" + actor + ".txt"
-            index += 1
-            files[name] = ""
-    files[name] += line + "\n"
-    i += 1
-
-buildMaster = False
-errors = 0
-index = -1
-for fil in files:
-    index += 1
-##    print "-----------------------", fil
-##    print files[fil],
-    rewrite = False
-    if os.path.exists(fil):
-        f = open(fil)
-        s = f.read()
-        f.close()
-        if s != files[fil]:
-            rewrite = True
-    else:
-        rewrite = True
-
-    if BUILDJUST != None:
-        if type(BUILDJUST) == type(0):
-            rewrite = BUILDJUST == index
-        else:
-            rewrite = BUILDJUST == fil
-
-    if rewrite:
-        buildMaster = True
-        print "-------->", fil, "is new or has changed -- writing to disk:"
-        print files[fil]
-        f = open(fil, "w")
-        f.write(files[fil])
-        f.close()
-        print "   rebuilding wav file"
-        wav = fil[:-4] + ".wav"
-        cmd = "rm " + wav
-        print cmd
-        os.system(cmd)
-        cmd = text2vox + " " + fil + " " + wav + " >> " + scene + ".log"
-        print cmd
-        os.system(cmd)
-        if not os.path.exists(wav):
-            print "***ERROR*** failed to build", wav, "-- renaming to force rebuild"
-            cmd = "mv " + fil + " " + fil[:-4] + "_error.txt"
-            print cmd
-            os.system(cmd)
-            errors += 1
-        else:
-            if CHANGES:
-                cmd = "mplayer " + fil[:-4] + ".wav"
-                print cmd
-                os.system(cmd)
-    else:
-        if BUILDJUST == None:
-            print "++++++++>", fil, "is unchanged"
-
-if (BUILDJUST == None) and (buildMaster and errors == 0):
-    print "Building master wav file"
-    cmd = "sox "
-    for fil in files:
-        print fil
-        cmd += fil[:-4] + ".wav "
-    cmd += scene + ".wav"
-    print cmd
-    os.system(cmd)
-
-    if BUILDPARTS:
-        print "Building parts for each actor"
-        for actor in actors:
-            someAudio = False
-            cmd = "sox "
-            for fil in files:
-                if not "_" + actor + ".txt" in fil:
-                    cmd += "-v 0 "
-                else:
-                    someAudio = True
-                cmd += fil[:-4] + ".wav "
-            cmd += scene + "_" + actor + ".wav"
-            if someAudio:
-                print cmd
-                os.system(cmd)
-
-if errors:
-    print errors, "errors encountered -- will not rebuild master"
-else:
-    if PLAY:
-        cmd = "mplayer " + scene + ".wav"
-        if type(PLAY) == type(0):
-            cmd += " -ss " + str(PLAY)
-        print cmd
-        os.system(cmd)
+main(scene)
